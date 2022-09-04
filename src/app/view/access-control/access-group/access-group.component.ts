06a485ac4b3c2777a8b363602d2b9345d80f4415
pianairco <pianairco@gmail.com>
2022-09-04 06:44:07 +0330

    tables

--- src/app/view/access-control/access-group/access-group.component.ts
+++ src/app/view/access-control/access-group/access-group.component.ts
@@ -33,8 +33,6 @@ export class AccessGroupComponent implements OnInit {
 export interface PeriodicElement {
   name: string;
   position: number;
-
-
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -47,6 +45,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 @Component({
   selector: 'Group-dialog',
+  styleUrls: ["./access-group.component.css"],
   templateUrl: 'group-dialog.component.html',
 })
 export class GroupDialogComponent {
@@ -59,6 +58,9 @@ export class GroupDialogComponent {
   myControl2 = new FormControl('');
   // @ts-ignore
   filteredOptions2: Observable<string[]>;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA2;
 
   ngOnInit(): void {
     this.filteredOptions = this.myControl.valueChanges.pipe(
@@ -87,3 +89,23 @@ export class GroupDialogComponent {
     return strings;
   }
 }
+
+export interface PeriodicElement2 {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA2: PeriodicElement2[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
--- src/app/view/alert-managements/alert-management/alert-management.component.ts
+++ src/app/view/alert-managements/alert-management/alert-management.component.ts
@@ -9,7 +9,8 @@ import {MatDialog} from "@angular/material/dialog";
 export class AlertManagementComponent implements OnInit {
   showModal = true;
   hide = true;
-
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor(public dialog: MatDialog) { }
 
@@ -30,3 +31,23 @@ export class AlertManagementComponent implements OnInit {
   templateUrl: 'alert-dialog.component.html',
 })
 export class AlertDialogComponent {}
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
